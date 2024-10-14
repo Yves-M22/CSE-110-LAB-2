2302791873ba0741f1f30f39b9724bd71d5b29ba
Yves-M22 <sebastianYSM12@gmail.com>
2024-10-14 01:46:43 -0700

    finished

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ThemeContext, themes } from "./themeContext";
+import { useState, useContext } from 'react';
 
 test('renders learn react link', () => {
-  render(<App />);
+    const [currentTheme, setCurrentTheme] = useState(themes.light);
+    
+    <ThemeContext.Provider value={currentTheme}>
+        render(<App />);
+    </ThemeContext.Provider>
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
